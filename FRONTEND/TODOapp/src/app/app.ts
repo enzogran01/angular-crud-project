f5aad0ceb8f5432a2a226bb04d29a9dc3d65e7ce
enzogran01 <yournarrator013@gmail.com>
2026-04-17 20:21:42 -0300

    test

--- FRONTEND/TODOapp/src/app/app.ts
+++ FRONTEND/TODOapp/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal, Input } from '@angular/core';
 import { Tarefa } from "./tarefa";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -10,27 +11,33 @@ import { Tarefa } from "./tarefa";
 export class App {
   protected readonly title = signal('TODOapp');
 
-  arrayDeTarefas: Tarefa[] = [];
-  constructor() {
+  arrayDeTarefas = signal<Tarefa[]>([]);
+  apiURL : string;
+
+  constructor(private http: HttpClient) {
+    this.apiURL = 'http://localhost:3000';
     this.READ_tarefas();
   }
 
   CREATE_tarefa(descricaoNovaTarefa: string) {
     var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
-    this.arrayDeTarefas.unshift(novaTarefa);
+    this.http.post<Tarefa>(`${this.apiURL}/api/post`, novaTarefa).subscribe(resultado => { console.log(resultado); this.READ_tarefas(); });
   }
-
 
   READ_tarefas() {
-    this.arrayDeTarefas = [
-      new Tarefa("Estudar Frameworks WEB", false),
-      new Tarefa("Comer Pizza", false),
-      new Tarefa("Ajudar meus pais", false)
-    ];
+    this.http.get<Tarefa[]>(`${this.apiURL}/api/getAll`).subscribe(resultado => this.arrayDeTarefas.set(resultado));
   }
 
-  DELETE_tarefa(tarefaAserRemovida : Tarefa) {
-    this.arrayDeTarefas.splice(this.arrayDeTarefas.indexOf(tarefaAserRemovida), 1);
+  DELETE_tarefa(tarefaAserRemovida: Tarefa) {
+    var indice = this.arrayDeTarefas().indexOf(tarefaAserRemovida);
+    var id = this.arrayDeTarefas()[indice]._id;
+    this.http.delete<Tarefa>(`${this.apiURL}/api/delete/${id}`).subscribe(resultado => { console.log(resultado); this.READ_tarefas(); });
   }
 
+  UPDATE_tarefa(tarefaAserModificada: Tarefa) {
+    var indice = this.arrayDeTarefas().indexOf(tarefaAserModificada);
+    var id = this.arrayDeTarefas()[indice]._id;
+    this.http.patch<Tarefa>(`${this.apiURL}/api/update/${id}`,
+    tarefaAserModificada).subscribe(resultado => { console.log(resultado); this.READ_tarefas(); });
+  }
 }
--- FRONTEND/TODOapp/src/app/tarefa.ts
+++ FRONTEND/TODOapp/src/app/tarefa.ts
@@ -1,8 +1,9 @@
 export class Tarefa {
- descricao: string;
- statusRealizada: boolean;
- constructor(_descricao: string, _statusRealizada: boolean) {
- this.descricao = _descricao;
- this.statusRealizada = _statusRealizada;
- }
+    _id : string | undefined ;
+    descricao: string;
+    statusRealizada: boolean;
+    constructor(_descricao: string, _statusRealizada: boolean) {
+        this.descricao = _descricao;
+        this.statusRealizada = _statusRealizada;
+    }
 }
